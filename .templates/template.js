19b5dd3c97850cd1d18dc72ccf93335f8428bf37
H3A4K <alex.perlock@gmail.com>
2026-02-04 15:06:08 -0500

    init

---
+++ .templates/template.js
@@ -0,0 +1,8 @@
+/**
+ * Author : Alexander Perlock + Ekaterina Uhalova
+ * MACID : perlocka + uhalovae
+ * Date Created : 
+ * Date Modified : 
+ * 
+ * Description of file
+ */
---
+++ event_listeners.js
@@ -0,0 +1,8 @@
+/**
+ * Author : Alexander Perlock + Ekaterina Uhalova
+ * MACID : perlocka + uhalovae
+ * Date Created : 04 02 26
+ * Date Modified : 04 02 26
+ * 
+ * Resposnible for the creation of all DOM event listeners
+ */
