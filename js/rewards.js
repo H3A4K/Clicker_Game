894f6ea9670d07507c98f652d1103acd0525e7c8
H3A4K <alex.perlock@gmail.com>
2026-02-18 16:47:46 -0500

    date changes

--- js/rewards.js
+++ js/rewards.js
@@ -2,7 +2,7 @@
  * Author : Alexander Perlock + Ekaterina Uhalova
  * MACID : perlocka + uhalovae
  * Date Created : 04 02 26
- * Date Modified : 04 02 26
+ * Date Modified : 13 02 26
  * 
  * Score instantiation and modification
  */
