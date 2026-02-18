957d68d5dbea322ba3051bba02c5f15d6afaef99
H3A4K <alex.perlock@gmail.com>
2026-02-18 17:18:54 -0500

    added cursor related achievements

--- js/rewards.js
+++ js/rewards.js
@@ -22,10 +22,12 @@ class Achievements {
         // }  else {
             this.rewards = [
                 // { name: "name", desc: "description", successor: "name of reward in unavailable (successors) array that is now achievable", requirement: () => what needs to happen to get this achievement }
-                { name: "First-Bubbles", desc: "", requirement: () => score >= 1, successor: "Ten-Bubbles" },
+                { name: "First-Bubbles", desc: "Aquire 1 bubble", requirement: () => score >= 1, successor: "Ten-Bubbles" }, 
+                { name: "Cursors", desc: "Aquire 1 cursor", requirement: () => shop.buildings[0].amount >= 1, successor: "Clicking Are We?" }
             ]
             this.unavailable = [
-                { name: "Ten-Bubbles", desc: "", requirement: () => score >= 10, successor: null },
+                { name: "Ten-Bubbles", desc: "Aquire 10 bubbles", requirement: () => score >= 10, successor: null },
+                { name: "Clicking Are We?", desc: "Aquire 10 cursor", requirement: () => shop.buildings[0].amount >= 10, successor: null },
             ];
             this.achieved = [];
         // }
